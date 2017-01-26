a2f59c12cb40e78179cf20f0d694c5f620b7c4a0
Abolaji Sulaiman Bisiriyu <abolajibisiriyu@gmail.com>
2017-01-26 13:14:54 +0100

    created SocialProvider model with migration and user relationship

---
+++ app/SocialProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SocialProvider extends Model
+{
+    //
+
+    public function user()
+    {
+        return $this->belongsTo(USer::class);
+    }
+}
